2e66012731214beeccc184351fd40023ff0e4529
Doctor <karuyanakaido24@gmail.com>
2024-08-28 21:15:50 +0700

    Upload demo source code tut01 Introduction

---
+++ Day 01 - Introduction and Motivations/src/day01_introduction/demo/exercise06_ComputeExpression/ComputeExpression.java
@@ -0,0 +1,7 @@
+package day01_introduction.demo.exercise6_ComputeExpression;
+
+public class ComputeExpression {
+	public static void main(String[] args) {
+		System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+	}
+}
